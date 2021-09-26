6a1575442617e8ab759106080407dd78b1260388
Rashadul <mohammed.haque@iths.se>
2021-09-26 11:11:42 +0200

    Tested with diff  value of input...

--- maxOfFour.js
+++ maxOfFour.js
@@ -1,28 +1,4 @@
-/*function maxOfFour(input1, input2, input3, input4) {
 
-    let output = input1;
-    if (output < input2) {
-        output = input2;
-    }
-
-    else if (output < input3) {
-        output = input3;
-    }
-
-    else if (output < input4) {
-        output = input4;
-    }
-
-    return output;
-}
-
-function main (){
-    let maximum = maxOfFour(5, 4, 7, 90);
-    console.log ('output of example-3 : ' + maximum);
-}
-
-main();
-*/
 
 console.log('------------') // To have a divider between result of two examples. Not related with func.
 
@@ -39,10 +15,10 @@ function maxOfFour(input1, input2, input3, input4)
 }
 function main()
 {
-    let input1 = 2;
-    let input2 = 41;
-    let input3= 30;
-    let input4 = 6;
+    let input1 = 7;
+    let input2 = 58;
+    let input3= 73;
+    let input4 = 16;
     let result = maxOfFour(input1, input2, input3, input4);
     console.log('Result of example-3 : ' + result);
 }
